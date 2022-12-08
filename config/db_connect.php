e3d55081526bc35ccd70e73cc2ea6008df902b49
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2022-12-08 15:02:11 +0545

    db connection

---
+++ config/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+function dbConnect()
+{
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "library_management_db";
+
+    return mysqli_connect($server, $username, $password, $dbname);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,28 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-echo "Hello World!";
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Library Management System</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
 
-?>
+<body>
+  <nav class="navbar bg-light sticky-top px-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./assets/image/logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+        Library Management
+      </a>
+      <a href="" class="d-flex">Admin</a>
+    </div>
+  </nav>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
